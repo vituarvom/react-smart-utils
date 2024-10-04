ae1ce019b3143ec5a944a575cd9bc1036e96a363
vighnesh <vignesh23birru@gmail.com>
2024-10-04 12:39:10 +0530

    Update hasKey function to check for object properties

--- hasKey.js
+++ hasKey.js
@@ -1,7 +1,3 @@
 function hasKey(obj, key) {
     return Object.prototype.hasOwnProperty.call(obj, key);
 }
-
-console.log(hasKey({ a: 1, b: 2 }, 'a'));  
-console.log(hasKey({ a: 1, b: 2 }, 'c'));  
-console.log(hasKey({ name: "vighnesh", age: 25 }, 'name')); 
