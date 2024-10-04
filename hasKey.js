a78044ba4b9aa91ffde6d55a7fe22587bd7a68f3
vighnesh <vignesh23birru@gmail.com>
2024-10-04 13:06:24 +0530

    added checks to validate input types, ensuring the function handles invalid arguments gracefully.

--- hasKey.js
+++ hasKey.js
@@ -1,18 +1,13 @@
-function hasKey(obj, key) {
-    
+function hasKey(obj, key){
+    // this is for to check the first parameter must be object
     if (typeof obj !== 'object' || obj === null) {
-        throw new TypeError('First argument must be a non-null object');
-    }
-
-    if (typeof key !== 'string') {
-        throw new TypeError('Second argument must be a string');
-    }
-
-    if (key.trim() === '') {
-        throw new Error('Key cannot be an empty string');
-    }
-
-    return Object.prototype.hasOwnProperty.call(obj, key);
-}
-
-
+         throw new TypeError('First argument must be an object');
+     }
+     // this is for to check the first parameter must be string
+     if (typeof key !== 'string' ||  key === null || key === undefined) {
+         throw new TypeError('Second argument must be a string');
+     }
+    
+     return Object.prototype.hasOwnProperty.call(obj, key);
+     
+     }
