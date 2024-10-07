6dcfd277e86b8fa3c8b32e266e5057d92d05f9dc
nikkivaddepelli <152060811+nikkivaddepelli@users.noreply.github.com>
2024-10-07 23:27:02 +0530

    feat(utils/string): add toUpperCase function (#85)
    * add toUpperCase function
    
    * i change the type to string
    
    * add tests and fix toUpperCase logics
    
    * Update src/services/error-handler.service.ts
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Bharat Dussa <bharatdussa@gmail.com>
    
    * fix formatting
    
    ---------
    
    Signed-off-by: Bharat Dussa <bharatdussa@gmail.com>
    Co-authored-by: Bharat Dussa <bharatdussa@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ src/services/error-handler.service.ts
@@ -0,0 +1,101 @@
+type ErrorType =
+  | "TypeError"
+  | "RangeError"
+  | "ReferenceError"
+  | "SyntaxError"
+  | "Error";
+
+type PrimitiveType =
+  | "string"
+  | "number"
+  | "boolean"
+  | "null"
+  | "undefined"
+  | "symbol"
+  | "bigint";
+
+type NonPrimitiveType =
+  | "object"
+  | "Function"
+  | "Array"
+  | "Date"
+  | "RegExp"
+  | "Map"
+  | "Set"
+  | "Promise"
+  | "Error";
+
+type Type = NonPrimitiveType | PrimitiveType;
+
+/**
+ * A class for handling errors in a structured and reusable way.
+ * It supports different types of errors and generates dynamic messages.
+ */
+class ErrorHandler {
+  private functionName: string;
+
+  constructor(functionName: string) {
+    this.functionName = functionName;
+  }
+
+  /**
+   * Generates and throws an error with a dynamic message.
+   *
+   * @param {ErrorType} errorType - The type of the error to throw.
+   * @param {string} expectedType - The expected data type or condition.
+   * @param {any} receivedValue - The actual value that caused the error.
+   */
+  public throwError(
+    errorType: ErrorType,
+    expectedType: string,
+    receivedValue: any
+  ): never {
+    const actualType = typeof receivedValue;
+    const errorMessage = `rsc: error from ${this.functionName}: Expected type ${expectedType} but received ${actualType}.`;
+
+    const errorClasses: { [key in ErrorType]: new (message: string) => Error } =
+      {
+        TypeError,
+        RangeError,
+        ReferenceError,
+        SyntaxError,
+        Error,
+      };
+    const ErrorConstructor = errorClasses[errorType] || Error;
+    throw new ErrorConstructor(errorMessage);
+  }
+
+  /**
+   * Throws a TypeError with a dynamic message.
+   * @param {string} expectedType - The expected data type or condition.
+   * @param {any} receivedValue - The actual value that caused the error.
+   */
+  public throwTypeError(expectedType: Type, receivedValue: any): never {
+    this.throwError("TypeError", expectedType.toString(), receivedValue);
+  }
+
+  /**
+   * Throws a RangeError with a dynamic message.
+   *
+   * @param {string} expectedRange - The expected value range or condition.
+   * @param {any} receivedValue - The actual value that caused the error.
+   */
+  public throwRangeError(expectedRange: string, receivedValue: any): never {
+    this.throwError("RangeError", expectedRange, receivedValue);
+  }
+
+  /**
+   * Throws a ReferenceError with a dynamic message.
+   *
+   * @param {string} expectedReference - The expected reference or condition.
+   * @param {any} receivedValue - The actual value that caused the error.
+   */
+  public throwReferenceError(
+    expectedReference: string,
+    receivedValue: any
+  ): never {
+    this.throwError("ReferenceError", expectedReference, receivedValue);
+  }
+}
+
+export default ErrorHandler;
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { toUpperCase } from "./string/toUpperCase/toUpperCase";
