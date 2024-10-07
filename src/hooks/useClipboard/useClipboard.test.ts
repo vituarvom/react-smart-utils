4238f5700837772e5008084cab8182ee3f42df5f
snehadomal <snehadomal80@gmail.com>
2024-10-07 23:53:54 +0530

    changes done

--- src/hooks/useClipboard/useClipboard.test.ts
+++ src/hooks/useClipboard/useClipboard.test.ts
@@ -2,13 +2,24 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import { useClipboard } from "./useClipboard";
 
 describe("useClipboard hook", () => {
-    it("should copied true and console the copied text", () => {
-        const { result } = renderHook(() => useClipboard("text"));
-
-        expect(result.current[1]).toBe(false);
-
-        act(() => result.current[0]());
-
-        expect(result.current[1]).toBe(true);
+  it("should set 'copied' to true when text is copied", async () => {
+    const mockWriteText = jest.fn();
+    Object.assign(navigator, {
+      clipboard: {
+        writeText: mockWriteText,
+      },
     });
+
+    const textToCopy = "test text";
+    const { result } = renderHook(() => useClipboard(textToCopy));
+
+    expect(result.current[1]).toBe(false);
+
+    await act(async () => {
+      await result.current[0]();
+    });
+
+    expect(result.current[1]).toBe(true);
+    expect(mockWriteText).toHaveBeenCalledWith(textToCopy);
+  });
 });
