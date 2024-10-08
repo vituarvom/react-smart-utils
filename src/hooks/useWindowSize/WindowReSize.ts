dfa2601fe0e11c84d980c9378bc4c371e817d728
rachhanari <racchanarendra123@gmail.com>
2024-10-08 18:29:28 +0530

    nameing and function changing

--- src/hooks/useWindowSize/WindowReSize.ts
+++ src/hooks/useWindowSize/WindowReSize.ts
@@ -9,10 +9,11 @@ import React, { useEffect, useState } from 'react';
  */
 export const useWindowReSize = () => {
     const [windowSize, setWindowSize] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: typeof window !== 'undefined' ? window.innerWidth : 0,
+        height: typeof window !== 'undefined' ? window.innerHeight : 0,
     });
 
+    
     useEffect(() => {
         const handleWindowSize = () => {
             try {
