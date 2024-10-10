d0d7c98edb1d6881339f367bad991657b403791a
bharatdussa <bharatdussa@gmail.com>
2024-10-10 20:08:11 +0530

    update declaration file and imports

---
+++ src/types/utils/function/clamp.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Clamps a number between a specified minimum and maximum range.
+ *
+ * @param {number} value - The number to be clamped.
+ * @param {number} min - The minimum allowable value.
+ * @param {number} max - The maximum allowable value.
+ * @returns {number} The clamped value, ensuring it falls between the min and max.
+ */
+export declare function clamp(value: number, min: number, max: number): number;
