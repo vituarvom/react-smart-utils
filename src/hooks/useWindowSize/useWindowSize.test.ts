4d094caabf919767aee582c66b67c4b7a1d3ef61
rachhanari <racchanarendra123@gmail.com>
2024-10-11 19:14:01 +0530

    issue done

---
+++ src/hooks/useWindowSize/useWindowSize.test.ts
@@ -0,0 +1,34 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useWindowSize from './useWindowSize';
+
+describe('useWindowSize hook', () => {
+  beforeEach(() => {
+    window.innerWidth = 1024;
+    window.innerHeight = 768;
+  });
+
+  it('should return the initial window size', () => {
+    const { result } = renderHook(() => useWindowSize());
+
+    expect(result.current.width).toBe(1024);
+    expect(result.current.height).toBe(768);
+  });
+
+  it('should update width and height on window resize', () => {
+    const { result } = renderHook(() => useWindowSize());
+
+  
+    act(() => {
+      window.innerHeight = 600;
+      window.innerWidth = 800;
+      window.dispatchEvent(new Event('resize'));
+    });
+
+    expect(result.current.width).toBe(800);
+    expect(result.current.height).toBe(600);
+  });
+})
+
+
+
+
---
+++ src/hooks/useWindowSize/useWindowSize.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+/**
+ * Custom hook to track and return the current window size.
+ * 
+ * @returns {WindowSize} The current width and height of the window.
+ */
+const useWindowSize = (): WindowSize => {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0,
+  });
+
+  useEffect(() => {
+    const handleWindowSize = () => {
+      try {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      } catch (err) {
+        console.error('Error while updating window size:', err);
+      }
+    };
+
+    window.addEventListener('resize', handleWindowSize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowSize);
+    };
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindowSize;
+
