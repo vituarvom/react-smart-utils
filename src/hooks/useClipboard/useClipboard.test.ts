658cf210acf02e7446e8d1461ca28d3682b1f1a8
snehadomal <snehadomal80@gmail.com>
2024-10-11 14:54:12 +0530

    changes done

--- src/hooks/useClipboard/useClipboard.test.ts
+++ src/hooks/useClipboard/useClipboard.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook,act } from "@testing-library/react-hooks";
 import { useClipboard } from "./useClipboard";
 
 describe("useClipboard hook", () => {
--- src/hooks/useClipboard/useClipboard.ts
+++ src/hooks/useClipboard/useClipboard.ts
@@ -2,8 +2,13 @@ import { useState } from 'react';
 
 export function useClipboard(text: any) {
     const [copied, setCopied] = useState(false);
-    const copy = () => {
-        setCopied(true);
-    };
+    const copy = async () => {
+        try {
+          await navigator.clipboard.writeText(text);
+          setCopied(true);
+        } catch (err) {
+          console.error('Failed to copy text: ', err);
+        }
+      };
     return [ copy, copied ] as const;
 }
