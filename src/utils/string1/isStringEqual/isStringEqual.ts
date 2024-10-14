9712d8c505c2991558a9be490f73bb0a02e1276d
nikita vaddepelli <nikitavaddepelli@gmail.com>
2024-10-14 23:14:21 +0530

    file changing add error

--- src/utils/string1/isStringEqual/isStringEqual.ts
+++ src/utils/string1/isStringEqual/isStringEqual.ts
@@ -1,3 +1,7 @@
-export function isStringEqual(str1: string, str2: string): boolean {
+export function isStringEqual(str1: string | null | undefined, str2: string | null | undefined): boolean {
+    if (str1 == null || str2 == null) {
+        throw new Error("Both parameters must be valid strings.");
+    }
+    
     return str1 === str2;
 }
