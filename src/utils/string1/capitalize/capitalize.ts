8927d942baed947e0d0e9fee5f208faf584802b2
doosabhulaxmi <bhulaxmi1999@gmail.com>
2024-10-16 20:10:55 +0530

    done changes

--- src/utils/string1/capitalize/capitalize.ts
+++ src/utils/string1/capitalize/capitalize.ts
@@ -5,5 +5,5 @@ export function capitalize(str: string): string {
     if (str.length === 0) {
         return str;
     }
-    return str.charAt(0).toLowerCase()+str.slice(1).toLowerCase();
+    return str.charAt(0).toUpperCase()+str.slice(1).toLowerCase();
 };
