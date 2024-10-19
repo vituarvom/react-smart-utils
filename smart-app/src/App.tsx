a3fb3e839714548d039a4619f1bb531790cb3250
Shubham Chimkar <shubhamchimkar5@gmail.com>
2024-10-19 23:51:43 +0530

    Add useHover hook and tests

--- smart-app/src/App.tsx
+++ smart-app/src/App.tsx
@@ -1,38 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { add } from 'react-smart-utils'
+import React from 'react';
+import UseHover from './Components/UseHover'; // Adjust the import path as necessary
 
-function App() {
-  const [count, setCount] = useState(0)
-
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        {  add()}
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ padding: '20px' }}>
+      <h1>Hover Detection Example</h1>
+      <UseHover />
+    </div>
+  );
+};
 
-export default App
+export default App;
