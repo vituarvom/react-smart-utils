f3e6c7360fe8d15b850eb7a53059bb868525fdd2
bharatdussa <bharatdussa@gmail.com>
2024-10-20 00:10:59 +0530

    add usage for hook

--- smart-app/src/App.tsx
+++ smart-app/src/App.tsx
@@ -1,37 +1,10 @@
-import { useState } from "react";
-import { useCountdown } from "react-smart-utils";
 import "./App.css";
+import UseCountDown from "./components/hooks/UseCountDown";
 
 function App() {
-  const [endTime, setEndTime] = useState<number | null>(null);
-
-  const countdown = useCountdown(endTime, {
-    interval: 1000,
-    format: 'mm:ss',
-    onTick: () => {
-      console.log(`Time remaining: ${countdown} seconds`);
-    },
-    onComplete: () => {
-      console.log("Countdown completed!");
-    },
-  });
-
-  const startCountdown = () => {
-    const futureTime = Date.now() + 120 * 5000; 
-    setEndTime(futureTime);
-  };
-
   return (
     <>
-      <div>
-        <h1>Countdown Timer</h1>
-        {countdown !== null ? (
-          <p>Time remaining: {countdown} seconds</p>
-        ) : (
-          <p>Countdown is not active.</p>
-        )}
-        <button onClick={startCountdown}>Start Countdown</button>
-      </div>
+      <UseCountDown />
     </>
   );
 }
