7fbc47dfb5623802fdf28d0cd71c91ff92cc5f8d
ReenaChidrala <chidralareena8@gmail.com>
2024-10-23 21:40:16 +0530

    feat(hook):UseLocalStorage (#138)
    * creating UseLocalStorage hook
    
    * completed UseLoaclStorage Hook
    
    * Apply suggestions from code review
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Bharat Dussa <bharatdussa@gmail.com>
    
    * code improvements
    
    ---------
    
    Signed-off-by: Bharat Dussa <bharatdussa@gmail.com>
    Co-authored-by: Bharat Dussa <bharatdussa@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- package/src/hooks/index.ts
+++ package/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { useToggle } from "./useToggle/useToggle";
 export { usePrevious } from "./usePrevious/usePrevious";
 export { useWindowSize } from "./useWindowSize/useWindowSize";
 export { useCountdown } from "./useCountdown/useCountdown"
+export { useLocalStorage } from "./useLocalStorage/useLocalStorage";
